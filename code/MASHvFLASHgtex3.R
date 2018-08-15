adc0756bf398eef1ac98b1baab1c5ea3e4d97b3e
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-08-15 15:04:51 -0400

    updates to MASHvFLASHgtex3 code and output

--- code/MASHvFLASHgtex3.R
+++ code/MASHvFLASHgtex3.R
@@ -5,7 +5,6 @@ devtools::load_all("/Users/willwerscheid/GitHub/ebnm/")
 
 library(mashr)
 source("./code/utils.R")
-source("./code/gtexanalysis.R")
 
 gtex <- readRDS(gzcon(url("https://github.com/stephenslab/gtexresults/blob/master/data/MatrixEQTLSumStats.Portable.Z.rds?raw=TRUE")))
 
@@ -15,15 +14,17 @@ random <- t(gtex$random.z)
 strong_data <- flash_set_data(strong, S = 1)
 random_data <- flash_set_data(random, S = 1)
 
+# The multi-tissue effects from the previous analysis will serve as
+#   the data-driven loadings:
 nn <- readRDS("./output/MASHvFLASHnn/fl.rds")
 multi <- c(2, 5, 6, 8, 11:13, 17, 22:25, 31)
-dd <- nn$EL[, multi]
-dd <- dd / rep(apply(dd, 2, max), each=n) # Normalize data-driven loadings
-
-
-## FLASH fit
-
 n <- nrow(strong)
+dd <- nn$EL[, multi]
+dd <- dd / rep(apply(dd, 2, max), each=n) # normalize
+
+
+## FLASH fit ------------------------------------------------------------
+
 canonical <- cbind(rep(1, n), diag(rep(1, n)))
 LL <- cbind(canonical, dd)
 
@@ -38,12 +39,9 @@ system.time(
 # 26 minutes (62 iterations)
 saveRDS(fl_random$gf, "./output/MASHvFLASHgtex3/flgf.rds")
 
-for (k in 2:45) {
-  fl_random$gf[[k]]$a <- min(fl_random$gf[[k]]$a, 1)
-}
-
+gf <- readRDS("./output/MASHvFLASHgtex3/flgf.rds")
 fl_final <- flash_add_fixed_loadings(strong_data, LL)
-ebnm_param_f = lapply(fl_random$gf, function(g) {list(g=g, fixg=TRUE)})
+ebnm_param_f = lapply(gf, function(g) {list(g=g, fixg=TRUE)})
 system.time(
   fl_final <- flash_backfit(strong_data,
                             fl_final,
@@ -52,7 +50,7 @@ system.time(
                             ebnm_param = list(f = ebnm_param_f, l = list()),
                             nullcheck = FALSE)
 )
-# 20 minutes (75 iterations)
+# 21 minutes (75 iterations)
 saveRDS(fl_final, "./output/MASHvFLASHgtex3/fl.rds")
 
 nsamp <- 200
@@ -65,7 +63,7 @@ system.time({
 saveRDS(fl_lfsr, "./output/MASHvFLASHgtex3/fllfsr.rds")
 
 
-# MASH fit
+# MASH fit --------------------------------------------------------------
 
 strong_data <- mash_set_data(t(strong), Shat = 1)
 random_data <- mash_set_data(t(random), Shat = 1)
@@ -83,13 +81,3 @@ system.time(
 )
 saveRDS(m_final, "./output/MASHvFLASHgtex3/m.rds")
 # 19 seconds
-
-
-m_lfsr <- t(get_lfsr(m_final))
-m_pm <- t(get_pm(m_final))
-fl_pm <- flash_get_fitted_values(fl_final)
-mash.v.flash <- compare_methods(fl_lfsr, m_lfsr, fl_pm, m_pm)
-for (n in mash.v.flash$diff_pms) {
-  plot_test(n, fl_lfsr, fl_pm, "FLASH")
-  plot_test(n, m_lfsr, m_pm, "MASH")
-}
